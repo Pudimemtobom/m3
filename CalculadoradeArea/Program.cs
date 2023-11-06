155805998f472a01a347e840a778b34222ee4e07
rai <51064041+Pudimemtobom@users.noreply.github.com>
2023-11-06 20:04:30 -0300

    Add files via upload

---
+++ CalculadoradeArea/Program.cs
@@ -0,0 +1,18 @@
+﻿    // Raisa Duarte de Aguiar - M3 - Calculadora de Área - 06/11/2023
+
+    class Program
+    {
+
+    private static void Main(string[] args)
+    {
+
+    Console.Clear();
+
+
+
+
+
+    }
+
+
+    }
